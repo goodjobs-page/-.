e66b1df1efd53389cb3e34e92b3f0074861c953d
wmgl5593 <87423727+jungjuheeer@users.noreply.github.com>
2021-09-27 16:30:21 +0900

    Initialize v0.15.x

---
+++ google/cloud/storage/client_write_object_test.cc
@@ -0,0 +1,170 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/make_unique.h"
+#include "google/cloud/storage/client.h"
+#include "google/cloud/storage/oauth2/google_credentials.h"
+#include "google/cloud/storage/retry_policy.h"
+#include "google/cloud/storage/testing/canonical_errors.h"
+#include "google/cloud/storage/testing/mock_client.h"
+#include "google/cloud/testing_util/assert_ok.h"
+#include <gmock/gmock.h>
+
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+namespace {
+
+using ::google::cloud::internal::make_unique;
+using ::google::cloud::storage::testing::canonical_errors::PermanentError;
+using ::google::cloud::storage::testing::canonical_errors::TransientError;
+using ::testing::_;
+using ::testing::HasSubstr;
+using ::testing::Invoke;
+using ::testing::Return;
+using ::testing::ReturnRef;
+using ms = std::chrono::milliseconds;
+
+/**
+ * Test the functions in Storage::Client related to writing objects.'Objects:
+ * *'.
+ */
+class WriteObjectTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    mock = std::make_shared<testing::MockClient>();
+    EXPECT_CALL(*mock, client_options())
+        .WillRepeatedly(ReturnRef(client_options));
+    client.reset(new Client{
+        std::shared_ptr<internal::RawClient>(mock),
+        ExponentialBackoffPolicy(std::chrono::milliseconds(1),
+                                 std::chrono::milliseconds(1), 2.0)});
+  }
+  void TearDown() override {
+    client.reset();
+    mock.reset();
+  }
+
+  std::shared_ptr<testing::MockClient> mock;
+  std::unique_ptr<Client> client;
+  ClientOptions client_options =
+      ClientOptions(oauth2::CreateAnonymousCredentials());
+};
+
+TEST_F(WriteObjectTest, WriteObject) {
+  std::string text = R"""({
+      "name": "test-bucket-name/test-object-name/1"
+})""";
+  auto expected = internal::ObjectMetadataParser::FromString(text).value();
+
+  EXPECT_CALL(*mock, CreateResumableSession(_))
+      .WillOnce(
+          Invoke([&expected](internal::ResumableUploadRequest const& request) {
+            EXPECT_EQ("test-bucket-name", request.bucket_name());
+            EXPECT_EQ("test-object-name", request.object_name());
+
+            auto mock = make_unique<testing::MockResumableUploadSession>();
+            using internal::ResumableUploadResponse;
+            EXPECT_CALL(*mock, done()).WillRepeatedly(Return(false));
+            EXPECT_CALL(*mock, next_expected_byte()).WillRepeatedly(Return(0));
+            EXPECT_CALL(*mock, UploadChunk(_))
+                .WillRepeatedly(Return(make_status_or(ResumableUploadResponse{
+                    "fake-url", 0, {}, ResumableUploadResponse::kInProgress})));
+            EXPECT_CALL(*mock, ResetSession())
+                .WillOnce(Return(make_status_or(ResumableUploadResponse{
+                    "fake-url", 0, {}, ResumableUploadResponse::kInProgress})));
+            EXPECT_CALL(*mock, UploadFinalChunk(_, _))
+                .WillOnce(
+                    Return(StatusOr<ResumableUploadResponse>(TransientError())))
+                .WillOnce(Return(make_status_or(ResumableUploadResponse{
+                    "fake-url", 0, expected, ResumableUploadResponse::kDone})));
+
+            return make_status_or(
+                std::unique_ptr<internal ::ResumableUploadSession>(
+                    std::move(mock)));
+          }));
+
+  auto stream = client->WriteObject("test-bucket-name", "test-object-name");
+  stream << "Hello World!";
+  stream.Close();
+  ObjectMetadata actual = stream.metadata().value();
+  EXPECT_EQ(expected, actual);
+}
+
+TEST_F(WriteObjectTest, WriteObjectTooManyFailures) {
+  Client client{std::shared_ptr<internal::RawClient>(mock),
+                LimitedErrorCountRetryPolicy(2),
+                ExponentialBackoffPolicy(std::chrono::milliseconds(1),
+                                         std::chrono::milliseconds(1), 2.0)};
+
+  auto returner = [](internal::ResumableUploadRequest const&) {
+    return StatusOr<std::unique_ptr<internal::ResumableUploadSession>>(
+        TransientError());
+  };
+  EXPECT_CALL(*mock, CreateResumableSession(_))
+      .WillOnce(Invoke(returner))
+      .WillOnce(Invoke(returner))
+      .WillOnce(Invoke(returner));
+
+  auto stream = client.WriteObject("test-bucket-name", "test-object-name");
+  EXPECT_TRUE(stream.bad());
+  EXPECT_FALSE(stream.metadata().status().ok());
+  EXPECT_EQ(TransientError().code(), stream.metadata().status().code())
+      << ", status=" << stream.metadata().status();
+}
+
+TEST_F(WriteObjectTest, WriteObjectPermanentFailure) {
+  auto returner = [](internal::ResumableUploadRequest const&) {
+    return StatusOr<std::unique_ptr<internal::ResumableUploadSession>>(
+        PermanentError());
+  };
+  EXPECT_CALL(*mock, CreateResumableSession(_)).WillOnce(Invoke(returner));
+  auto stream = client->WriteObject("test-bucket-name", "test-object-name");
+  EXPECT_TRUE(stream.bad());
+  EXPECT_FALSE(stream.metadata().status().ok());
+  EXPECT_EQ(PermanentError().code(), stream.metadata().status().code())
+      << ", status=" << stream.metadata().status();
+}
+
+TEST_F(WriteObjectTest, WriteObjectPermanentSessionFailurePropagates) {
+  testing::MockResumableUploadSession* mock_session =
+      new testing::MockResumableUploadSession;
+  auto returner = [mock_session](internal::ResumableUploadRequest const&) {
+    return StatusOr<std::unique_ptr<internal::ResumableUploadSession>>(
+        std::unique_ptr<internal::ResumableUploadSession>(mock_session));
+  };
+  std::string const empty;
+  EXPECT_CALL(*mock, CreateResumableSession(_)).WillOnce(Invoke(returner));
+  EXPECT_CALL(*mock_session, UploadChunk(_))
+      .WillRepeatedly(Return(PermanentError()));
+  EXPECT_CALL(*mock_session, done()).WillRepeatedly(Return(false));
+  EXPECT_CALL(*mock_session, session_id()).WillRepeatedly(ReturnRef(empty));
+  auto stream = client->WriteObject("test-bucket-name", "test-object-name");
+
+  // make sure it is actually sent
+  std::vector<char> data(client_options.upload_buffer_size() + 1, 'X');
+  stream.write(data.data(), data.size());
+  EXPECT_TRUE(stream.bad());
+  stream.Close();
+  EXPECT_FALSE(stream.metadata());
+  EXPECT_EQ(PermanentError().code(), stream.metadata().status().code())
+      << ", status=" << stream.metadata().status();
+}
+
+}  // namespace
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/storage/internal/curl_resumable_upload_session_test.cc
@@ -0,0 +1,163 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/internal/curl_resumable_upload_session.h"
+#include "google/cloud/storage/internal/curl_client.h"
+#include "google/cloud/storage/oauth2/google_credentials.h"
+#include "google/cloud/testing_util/assert_ok.h"
+#include <gmock/gmock.h>
+
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+namespace internal {
+namespace {
+
+using ::testing::_;
+using ::testing::Invoke;
+
+class MockCurlClient : public CurlClient {
+ public:
+  static std::shared_ptr<MockCurlClient> Create() {
+    return std::make_shared<MockCurlClient>();
+  }
+
+  MockCurlClient()
+      : CurlClient(ClientOptions(oauth2::CreateAnonymousCredentials())) {}
+
+  MOCK_METHOD1(UploadChunk,
+               StatusOr<ResumableUploadResponse>(UploadChunkRequest const&));
+  MOCK_METHOD1(QueryResumableUpload, StatusOr<ResumableUploadResponse>(
+                                         QueryResumableUploadRequest const&));
+};
+
+TEST(CurlResumableUploadSessionTest, Simple) {
+  auto mock = MockCurlClient::Create();
+  std::string test_url = "http://invalid.example.com/not-used-in-mock";
+  CurlResumableUploadSession session(mock, test_url);
+
+  std::string const payload = "test payload";
+  auto const size = payload.size();
+
+  EXPECT_FALSE(session.done());
+  EXPECT_EQ(0, session.next_expected_byte());
+  EXPECT_CALL(*mock, UploadChunk(_))
+      .WillOnce(Invoke([&](UploadChunkRequest const& request) {
+        EXPECT_EQ(test_url, request.upload_session_url());
+        EXPECT_EQ(payload, request.payload());
+        EXPECT_EQ(0, request.source_size());
+        EXPECT_EQ(0, request.range_begin());
+        return make_status_or(ResumableUploadResponse{
+            "", size - 1, {}, ResumableUploadResponse::kInProgress});
+      }))
+      .WillOnce(Invoke([&](UploadChunkRequest const& request) {
+        EXPECT_EQ(test_url, request.upload_session_url());
+        EXPECT_EQ(payload, request.payload());
+        EXPECT_EQ(2 * size, request.source_size());
+        EXPECT_EQ(size, request.range_begin());
+        return make_status_or(ResumableUploadResponse{
+            "", 2 * size - 1, {}, ResumableUploadResponse::kDone});
+      }));
+
+  auto upload = session.UploadChunk(payload);
+  EXPECT_STATUS_OK(upload);
+  EXPECT_EQ(size - 1, upload->last_committed_byte);
+  EXPECT_EQ(size, session.next_expected_byte());
+  EXPECT_FALSE(session.done());
+
+  upload = session.UploadFinalChunk(payload, 2 * size);
+  EXPECT_STATUS_OK(upload);
+  EXPECT_EQ(2 * size - 1, upload->last_committed_byte);
+  EXPECT_EQ(2 * size, session.next_expected_byte());
+  EXPECT_TRUE(session.done());
+}
+
+TEST(CurlResumableUploadSessionTest, Reset) {
+  auto mock = MockCurlClient::Create();
+  std::string url1 = "http://invalid.example.com/not-used-in-mock-1";
+  std::string url2 = "http://invalid.example.com/not-used-in-mock-2";
+  CurlResumableUploadSession session(mock, url1);
+
+  std::string const payload = "test payload";
+  auto const size = payload.size();
+
+  EXPECT_EQ(0, session.next_expected_byte());
+  EXPECT_CALL(*mock, UploadChunk(_))
+      .WillOnce(Invoke([&](UploadChunkRequest const&) {
+        return make_status_or(ResumableUploadResponse{
+            "", size - 1, {}, ResumableUploadResponse::kInProgress});
+      }))
+      .WillOnce(Invoke([&](UploadChunkRequest const&) {
+        return StatusOr<ResumableUploadResponse>(
+            AsStatus(HttpResponse{308, "uh oh", {}}));
+      }));
+  const ResumableUploadResponse resume_response{
+      url2, 2 * size - 1, {}, ResumableUploadResponse::kInProgress};
+  EXPECT_CALL(*mock, QueryResumableUpload(_))
+      .WillOnce(Invoke([&](QueryResumableUploadRequest const& request) {
+        EXPECT_EQ(url1, request.upload_session_url());
+        return make_status_or(resume_response);
+      }));
+
+  auto upload = session.UploadChunk(payload);
+  EXPECT_EQ(size, session.next_expected_byte());
+  upload = session.UploadChunk(payload);
+  EXPECT_FALSE(upload.ok());
+  EXPECT_EQ(size, session.next_expected_byte());
+  EXPECT_EQ(url1, session.session_id());
+
+  session.ResetSession();
+  EXPECT_EQ(2 * size, session.next_expected_byte());
+  EXPECT_EQ(url2, session.session_id());
+  StatusOr<ResumableUploadResponse> const& last_response =
+      session.last_response();
+  ASSERT_STATUS_OK(last_response);
+  EXPECT_EQ(last_response.value(), resume_response);
+}
+
+TEST(CurlResumableUploadSessionTest, SessionUpdatedInChunkUpload) {
+  auto mock = MockCurlClient::Create();
+  std::string url1 = "http://invalid.example.com/not-used-in-mock-1";
+  std::string url2 = "http://invalid.example.com/not-used-in-mock-2";
+  CurlResumableUploadSession session(mock, url1);
+
+  std::string const payload = "test payload";
+  auto const size = payload.size();
+
+  EXPECT_EQ(0, session.next_expected_byte());
+  EXPECT_CALL(*mock, UploadChunk(_))
+      .WillOnce(Invoke([&](UploadChunkRequest const&) {
+        return make_status_or(ResumableUploadResponse{
+            "", size - 1, {}, ResumableUploadResponse::kInProgress});
+      }))
+      .WillOnce(Invoke([&](UploadChunkRequest const&) {
+        return make_status_or(ResumableUploadResponse{
+            url2, 2 * size - 1, {}, ResumableUploadResponse::kInProgress});
+      }));
+
+  auto upload = session.UploadChunk(payload);
+  EXPECT_EQ(size, session.next_expected_byte());
+  upload = session.UploadChunk(payload);
+  EXPECT_STATUS_OK(upload);
+  EXPECT_EQ(2 * size, session.next_expected_byte());
+  EXPECT_EQ(url2, session.session_id());
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
